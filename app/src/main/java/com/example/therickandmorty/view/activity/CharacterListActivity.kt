c1fa4ed2fed0e63f52059a2e45887dffec0948b6
koro <koro.nn99@gmail.com>
2021-06-04 01:23:17 -0700

    set character list fragment on activity

--- app/src/main/java/com/example/therickandmorty/view/activity/CharacterListActivity.kt
+++ app/src/main/java/com/example/therickandmorty/view/activity/CharacterListActivity.kt
@@ -3,10 +3,18 @@ package com.example.therickandmorty.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.therickandmorty.R
+import com.example.therickandmorty.view.fragment.CharacterListFragment
 
 class CharacterListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container, CharacterListFragment())
+                .commit()
+        }
     }
 }
