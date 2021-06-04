37f49e7cfb762a83f9055193926bdd9a14849606
koro <koro.nn99@gmail.com>
2021-06-04 00:45:01 -0700

    fetch character images in ViewModel

--- app/src/main/java/com/example/therickandmorty/view_model/CharacterListViewModel.kt
+++ app/src/main/java/com/example/therickandmorty/view_model/CharacterListViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.therickandmorty.model.Common
 import com.example.therickandmorty.model.character.CharacterHeadline
+import com.example.therickandmorty.repository.CharacterImageRepository
 import com.example.therickandmorty.repository.CharactersApiRepository
 import com.example.therickandmorty.repository.CharactersRepository
 import kotlinx.coroutines.Dispatchers
@@ -14,6 +15,7 @@ class CharacterListViewModel : ViewModel() {
 
     private val charactersApiRepository = CharactersApiRepository()
     private val charactersRepository = CharactersRepository()
+    private val characterImageRepository = CharacterImageRepository()
 
     private var characters: MutableList<CharacterHeadline> = mutableListOf()
     var liveCharacters: MutableLiveData<MutableList<CharacterHeadline>> = MutableLiveData()
@@ -27,20 +29,29 @@ class CharacterListViewModel : ViewModel() {
 
         viewModelScope.launch(Dispatchers.IO) {
 
-            // fetching characters API URL
+            // TASK-1: fetching characters API URL
             val charactersApi =
                 charactersApiRepository.fetchCharactersApiUrl()
                     ?: return@launch //TODO error handling
             Common.charactersApiUrl = charactersApi // to use in other Activity
 
-            // fetching characters data(except image)
+            // TASK-2: fetching characters data(except image)
             characters = charactersRepository.fetchCharacters(charactersApi) ?: return@launch //TODO error handling
             viewModelScope.launch(Dispatchers.Main) {
                 liveCharacters.value = characters
             }
 
-            // fetching character images
+            // TASK-3: fetching character images
+            for (i in characters.indices) {
+                viewModelScope.launch(Dispatchers.IO) {
+                    val urlStr = characters[i].imageUrl
+                    val image = characterImageRepository.fetchImage(urlStr)
+                    viewModelScope.launch(Dispatchers.Main) {
+                        characters[i].image = image
+                        liveCharacters.value = characters
+                    }
+                }
+            }
         }
-
     }
 }
