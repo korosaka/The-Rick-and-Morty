c9c66056484f5922a8f16b5323e534f52a535d8a
koro <koro.nn99@gmail.com>
2021-06-04 12:19:42 -0700

    use fragment in activity

--- app/src/main/java/com/example/therickandmorty/view/activity/CharacterDetailActivity.kt
+++ app/src/main/java/com/example/therickandmorty/view/activity/CharacterDetailActivity.kt
@@ -3,10 +3,19 @@ package com.example.therickandmorty.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.therickandmorty.R
+import com.example.therickandmorty.view.fragment.CharacterDetailFragment
 
 class CharacterDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_character_detail_activity)
+
+        if (savedInstanceState == null) {
+            val characterId = intent.getStringExtra("character_id")
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container, CharacterDetailFragment.newInstance(characterId))
+                .commit()
+        }
     }
 }
